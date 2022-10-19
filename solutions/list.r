566f06c10a2d859d126980970ef419b284f7c2b6
Wim Cardoen <wim.cardoen@utah.edu>
2022-10-19 12:35:56 -0600

    Solutions for the list ex.

--- solutions/list.r
+++ solutions/list.r
@@ -1,31 +1,49 @@
-# Exercise
-# ========
-mylst <- list(
-              list(
-                   seq(from=4,to=40,by=5),
-                   "hello world",
-                    list(
-                         matrix(100:119,nrow=5),
-                         "bye",
-                         c(7,13,17),
-                         list(451,-1,-17) 
-		        )
-                  )
-             )
+# Exercise 1:
+# ==========
+lstex1 <- list(
+               list(
+                    seq(from=4,to=40,by=5),
+                    "hello world",
+                     list(
+                          matrix(100:119,nrow=5),
+                          "bye",
+                          c(7,13,17),
+                          list(451,-1,-17) 
+	 	         )
+                   )
+              )
 
 # Extract (7,13):
-mylst[[1]][[3]][[3]][c(1,2)]
+lstex1[[1]][[3]][[3]][c(1,2)]
 
 # Extract the second column of the matrix as a matrix
-mylst[[1]][[3]][[1]][,2,drop=FALSE]
+lstex1[[1]][[3]][[1]][,2,drop=FALSE]
 
 # Extract -1 as a scalar
-mylst[[1]][[3]][[4]][[2]]
+lstex1[[1]][[3]][[4]][[2]]
 
 # Extract -1 as a list
-mylst[[1]][[3]][[4]][2]
+lstex1[[1]][[3]][[4]][2]
 
 # Extract ALL numeric values 
-res <- unlist(mylst)
+res <- unlist(lstex1)
 res <- as.double(res)
 res[!is.na(res)]
+
+
+# Exercise 2:
+# ==========
+lstex2 <- list(1:10,
+               matrix(seq(from=1,to=20), nrow=4),
+               5+3i,
+               list('a','d','b'),
+               "UoU",
+               c(T,F,T,T,T),
+               "Hello"
+	      ) 
+lstex2[[7]] <- NULL
+lstex2[[5]] <- "Hello UoU"
+lstex2[[4]] <- list('a','b','c','d')
+lstex2[[3]] <- NULL
+lstex2[[1]] <- NULL
+lstex2
