4c0e5f4dc761a8ea7a21a1eca9ac9624b409b492
Wim Cardoen <wim.cardoen@utah.edu>
2022-11-17 21:05:24 -0700

    script to create picture of normal distro

--- img/gauss.py
+++ img/gauss.py
@@ -4,15 +4,12 @@ import matplotlib.pyplot as plt
 
 loc = 5.0
 scale = 2.0
+pt = 8.0
 
 x = np.linspace(norm.ppf(0.001,loc=loc,scale=scale),norm.ppf(0.999,loc=loc,scale=scale),1001)
 y = norm.pdf(x, loc=loc,scale=scale)
 yt= np.max(y)
-print(f"Max. Value y:{yt}")
-pt = 8.0
 pval = norm.cdf(pt, loc=loc, scale=scale)
-print(f"Pdf at {pt} is {norm.pdf(pt, loc=loc, scale=scale)}")
-print(f"Cumulative p at {pt} is {pval}")
 fig, ax = plt.subplots(1,1)
 ax.set_title(r'$X \, \sim \, N(\mu=5,\sigma^2=4)$')
 ax.plot(x, y, 'r-', lw=3, alpha=0.6, label='norm pdf')
